d2446109f5f536691745f37dd59b470cd07c4714
rial01 <rial01@126.com>
2019-07-01 14:48:35 +0800

    init

---
+++ src/build/CMakeFiles/test.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/test.dir/lib.c.o"
+  "/home/rial/workspaces/auto-test/gitspace/bin/libtest.pdb"
+  "/home/rial/workspaces/auto-test/gitspace/bin/libtest.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ test/build/CMakeFiles/testunit.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/testunit.dir/gtestDemo.o"
+  "/home/rial/workspaces/auto-test/gitspace/bin/testunit.pdb"
+  "/home/rial/workspaces/auto-test/gitspace/bin/testunit"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/testunit.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
